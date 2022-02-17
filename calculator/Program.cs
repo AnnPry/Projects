b0b7a15185e08d4c6c293da014bbd666074648f6
AnnPry <87533389+AnnPry@users.noreply.github.com>
2022-02-17 22:26:45 +0200

    .

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -6,187 +6,161 @@ namespace calculator
     {
         static void Main(string[] args)
         {
-            while (true)
-            {
-                GetOperationFromUser();
-                string oper = Console.ReadLine();
-                
-                //if(oper == "m" || oper == "d")
-                //{
+            GetOperationFromUser("Operation: ");
 
-                //}
+            #region calculator 1
+            //if (Console.ReadKey().Key == ConsoleKey.M)
+            //{                    
+            //    Console.ReadLine();
 
-                double n1 = GetDoubleFromUser("Number 1: ");
-                
-                double n2 = GetDoubleFromUser("Number 2: ");
-                
-                string result = "Result: ";
-                string Error = "";
+            //    Console.Write("Number: ");
+            //    bool num = double.TryParse(Console.ReadLine(), out double n);
 
-                switch (oper)
-                {
-                    case "+":
-                        result += n1 + n2;
-                        break;
-                    case "-":
-                        result += n1 - n2;
-                        break;
-                    case "/":
-                        result += n1 / n2;
-                        break;
-                    case "*":
-                        result += n1 * n2;
-                        break;
-                    case "%":
-                        result += n1 % n2;
-                        break;
-                    default:
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("It isn't possible in the program");
-                        Console.ResetColor();
-                        break;
-                }
+            //    if(n < 0)
+            //    {
+            //        double res = n + -(n + n);
+            //        Console.WriteLine($"|{res}|");
+            //    }
+            //    else if(n >= 0)
+            //    {
+            //        Console.WriteLine($"Result: |{n}|");
+            //    }
+            //    else
+            //    {
+            //        Console.ForegroundColor = ConsoleColor.Red;
+            //        Console.WriteLine("Error");
+            //        Console.ResetColor();
+            //    }
+            //}
+            //else
+            //{
+            //    Console.Clear();
+            //}
+            //Console.ResetColor();
 
-                if (Error.Length == 0)
-                {
-                    Console.WriteLine(result);
-                }
-                else
-                {
-                    Console.WriteLine(result);
-                    RedErrorString();
-                }
-                #region calculator 1
-                //if (Console.ReadKey().Key == ConsoleKey.M)
-                //{                    
-                //    Console.ReadLine();
+            //Console.ForegroundColor = ConsoleColor.Magenta;
+            //Console.WriteLine("Press D to do degree of number");
 
-                //    Console.Write("Number: ");
-                //    bool num = double.TryParse(Console.ReadLine(), out double n);
+            //if (Console.ReadKey().Key == ConsoleKey.D)
+            //{
+            //    Console.ReadLine();
 
-                //    if(n < 0)
-                //    {
-                //        double res = n + -(n + n);
-                //        Console.WriteLine($"|{res}|");
-                //    }
-                //    else if(n >= 0)
-                //    {
-                //        Console.WriteLine($"Result: |{n}|");
-                //    }
-                //    else
-                //    {
-                //        Console.ForegroundColor = ConsoleColor.Red;
-                //        Console.WriteLine("Error");
-                //        Console.ResetColor();
-                //    }
-                //}
-                //else
-                //{
-                //    Console.Clear();
-                //}
-                //Console.ResetColor();
+            //    Console.Write("Number: ");
+            //    bool num3 = double.TryParse(Console.ReadLine(), out double n3);
 
-                //Console.ForegroundColor = ConsoleColor.Magenta;
-                //Console.WriteLine("Press D to do degree of number");
+            //    Console.Write("Degree of number: ");
+            //    bool num4 = int.TryParse(Console.ReadLine(), out int n4);
 
-                //if (Console.ReadKey().Key == ConsoleKey.D)
-                //{
-                //    Console.ReadLine();
+            //    if (num3 == true && num4 == true)
+            //    {
+            //        Console.WriteLine($"Result: {Math.Pow(n3, n4)}");
+            //    }
+            //    else
+            //    {
+            //        Console.ForegroundColor = ConsoleColor.Red;
+            //        Console.WriteLine("Error");
+            //        Console.ResetColor();      
+            //    }
+            //}
+            //else
+            //{
+            //    Console.Clear();
+            //}
+            //Console.ResetColor();
 
-                //    Console.Write("Number: ");
-                //    bool num3 = double.TryParse(Console.ReadLine(), out double n3);
+            //Console.ForegroundColor = ConsoleColor.White;
+            //Console.Write("Number 1: ");
+            //bool num1 = double.TryParse(Console.ReadLine(), out double n1);
 
-                //    Console.Write("Degree of number: ");
-                //    bool num4 = int.TryParse(Console.ReadLine(), out int n4);
+            //Console.Write("Number 2: ");
+            //bool num2 = double.TryParse(Console.ReadLine(), out double n2);
 
-                //    if (num3 == true && num4 == true)
-                //    {
-                //        Console.WriteLine($"Result: {Math.Pow(n3, n4)}");
-                //    }
-                //    else
-                //    {
-                //        Console.ForegroundColor = ConsoleColor.Red;
-                //        Console.WriteLine("Error");
-                //        Console.ResetColor();      
-                //    }
-                //}
-                //else
-                //{
-                //    Console.Clear();
-                //}
-                //Console.ResetColor();
+            //Console.Write("Operation: ");
+            //string oper = Console.ReadLine();
 
-                //Console.ForegroundColor = ConsoleColor.White;
-                //Console.Write("Number 1: ");
-                //bool num1 = double.TryParse(Console.ReadLine(), out double n1);
+            //string result = "Result: ";
+            //string Error = "";
 
-                //Console.Write("Number 2: ");
-                //bool num2 = double.TryParse(Console.ReadLine(), out double n2);
+            //if (num1 == false && num2 == false)
+            //{
+            //    Error += "Error";
+            //}
 
-                //Console.Write("Operation: ");
-                //string oper = Console.ReadLine();
+            //switch (oper)
+            //{
+            //    case "+":
+            //        result += n1 + n2;
+            //        break;
+            //    case "-":
+            //        result += n1 - n2;
+            //        break;
+            //    case "/":
+            //        result += n1 / n2;
+            //        break;
+            //    case "*":
+            //        result += n1 * n2;
+            //        break;
+            //    case "%":
+            //        result += n1 % n2;
+            //        break;
+            //    default:
+            //        Console.ForegroundColor = ConsoleColor.Red;
+            //        Console.WriteLine("Error");
+            //        Console.ResetColor();
+            //        break;
+            //}
+            //Console.ResetColor();
 
-                //string result = "Result: ";
-                //string Error = "";
+            //Console.ForegroundColor = ConsoleColor.White;
+            //if (Error.Length == 0)
+            //{
+            //    Console.WriteLine(result);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(result);
+            //    Console.ForegroundColor = ConsoleColor.Red;
+            //    Console.WriteLine(Error);
+            //    Console.ResetColor();
 
-                //if (num1 == false && num2 == false)
-                //{
-                //    Error += "Error";
-                //}
+            //}
+            //Console.ResetColor();
 
-                //switch (oper)
-                //{
-                //    case "+":
-                //        result += n1 + n2;
-                //        break;
-                //    case "-":
-                //        result += n1 - n2;
-                //        break;
-                //    case "/":
-                //        result += n1 / n2;
-                //        break;
-                //    case "*":
-                //        result += n1 * n2;
-                //        break;
-                //    case "%":
-                //        result += n1 % n2;
-                //        break;
-                //    default:
-                //        Console.ForegroundColor = ConsoleColor.Red;
-                //        Console.WriteLine("Error");
-                //        Console.ResetColor();
-                //        break;
-                //}
-                //Console.ResetColor();
+            //switch (Console.ReadKey().Key)
+            //{
+            //    case ConsoleKey.M:
+            //        Console.ReadLine();
 
-                //Console.ForegroundColor = ConsoleColor.White;
-                //if (Error.Length == 0)
-                //{
-                //    Console.WriteLine(result);
-                //}
-                //else
-                //{
-                //    Console.WriteLine(result);
-                //    Console.ForegroundColor = ConsoleColor.Red;
-                //    Console.WriteLine(Error);
-                //    Console.ResetColor();
+            //        double n = GetDoubleFromUser("Number: ");
 
-                //}
-                //Console.ResetColor();
-                #endregion
+            //        if (n < 0 || n >= 0)
+            //        {
+            //            Console.WriteLine($"|{Math.Abs(n)}|");
 
-                Console.ForegroundColor = ConsoleColor.Cyan;
-                Console.WriteLine("Press S to stop and exit");
+            //        }
+            //        else
+            //        {
+            //            RedErrorString();
+            //        }
+            //        break;
+            //    case ConsoleKey.D:
+            //        Console.ReadLine();
 
-                if (Console.ReadKey().Key == ConsoleKey.S)
-                {
-                    break;
-                }
-                Console.ResetColor();
+            //        double n1 = GetDoubleFromUser("Number: ");
 
-                Console.Clear();                
-            }
-            
+            //        Console.Write("Degree of number: ");
+            //        bool num2 = int.TryParse(Console.ReadLine(), out int n2);
+
+            //        Console.WriteLine($"Result: {Math.Pow(n1, n2)}");
+
+            //        if (num2 == false)
+            //        {
+            //            RedErrorString();
+            //        }
+            //        break;
+            //}
+
+            #endregion
         }
         static double GetDoubleFromUser(string question)
         {
@@ -209,43 +183,128 @@ namespace calculator
             return number;
 
         }  
-        static void GetOperationFromUser()
+        static string GetOperationFromUser(string question)
         {
-            Console.Write("Operation: ");
+            bool sym = false;
+            string symbol = "";
 
-            switch (Console.ReadKey().Key)
+            while (!sym)
             {
-                case ConsoleKey.M:
-                    Console.ReadLine();
+                Console.Write(question);
+                string oper = Console.ReadLine();
+                sym = string.Equals(question, symbol);
 
-                    double n = GetDoubleFromUser("Number: ");
+                if (oper == "/"|| oper == "*"|| oper == "+"|| oper == "-"|| oper == "%")
+                {
+                    double n1 = GetDoubleFromUser("Number 1: ");
 
-                    if (n < 0 || n >= 0)
+                    double n2 = GetDoubleFromUser("Number 2: ");
+
+                    string result = "Result: ";
+                    string Error = "";
+
+                    switch (oper)
                     {
-                        Console.WriteLine($"|{Math.Abs(n)}|");
+                        case "+":
+                            result += n1 + n2;
+                            break;
+                        case "-":
+                            result += n1 - n2;
+                            break;
+                        case "/":
+                            result += n1 / n2;
+                            break;
+                        case "*":
+                            result += n1 * n2;
+                            break;
+                        case "%":
+                            result += n1 % n2;
+                            break;
+                        default:
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("It isn't possible in the program");
+                            Console.ResetColor();
+                            break;
+                    }
+               
 
+                    if (Error.Length == 0)
+                    {
+                        Console.WriteLine(result);
                     }
                     else
                     {
+                        Console.WriteLine(result);
                         RedErrorString();
                     }
-                    break;
-                case ConsoleKey.D:
-                    Console.ReadLine();
+                    
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine("Press S to stop and exit");
 
-                    double n1 = GetDoubleFromUser("Number: ");
-
-                    Console.Write("Degree of number: ");
-                    bool num2 = int.TryParse(Console.ReadLine(), out int n2);
-
-                    Console.WriteLine($"Result: {Math.Pow(n1, n2)}");
-
-                    if (num2 == false)
+                    if (Console.ReadKey().Key == ConsoleKey.S)
                     {
-                        RedErrorString();
+                        break;
                     }
-                    break;
+                    Console.ResetColor();
+
+                    Console.Clear();
+                }
+                else if (oper == "m" || oper == "M" || oper == "d" || oper == "D")
+                {
+                    Console.Write("Repeat letter: ");
+                    switch (Console.ReadKey().Key)
+                    {
+                        case ConsoleKey.M:
+                            Console.ReadLine();
+
+                            double n = GetDoubleFromUser("Number: ");
+
+                            if (n < 0 || n >= 0)
+                            {
+                                Console.WriteLine($"|{Math.Abs(n)}|");
+
+                            }
+                            else
+                            {
+                                RedErrorString();
+                            }
+                            break;
+                        case ConsoleKey.D:
+                            Console.ReadLine();
+
+                            double n1 = GetDoubleFromUser("Number: ");
+
+                            Console.Write("Degree of number: ");
+                            bool num2 = int.TryParse(Console.ReadLine(), out int n2);
+
+                            Console.WriteLine($"Result: {Math.Pow(n1, n2)}");
+
+                            if (num2 == false)
+                            {
+                                RedErrorString();
+                            }
+                            break;
+                    }
+
+                    Console.ForegroundColor = ConsoleColor.Cyan;
+                    Console.WriteLine("Press S to stop and exit");
+
+                    if (Console.ReadKey().Key == ConsoleKey.S)
+                    {
+                        break;
+                    }
+                    Console.ResetColor();
+
+                    Console.Clear();
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Input isn't a symbol! Plese try again!");
+                    Console.ResetColor();
+                }
             }
+                return symbol;
         }
         
         static void RedErrorString()
@@ -256,5 +315,3 @@ namespace calculator
         }
     }
 }
-
-
