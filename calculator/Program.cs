1eb4c9a9c03dd11aa5bfb86e43fdb713f867e331
AnnPry <87533389+AnnPry@users.noreply.github.com>
2022-03-11 18:26:31 +0200

    .

--- calculator/Program.cs
+++ calculator/Program.cs
@@ -39,49 +39,7 @@ namespace calculator
 
                 if (oper == "/" || oper == "*" || oper == "+" || oper == "-" || oper == "%" || oper == "d")
                 {
-                    double n1 = GetDoubleFromUser("Number 1: ");
-
-                    double n2 = GetDoubleFromUser("Number 2: ");
-
-                    string result = "Result: ";
-                    string Error = "";
-
-                    switch (oper)
-                    {
-                        case "+":
-                            result += n1 + n2;
-                            break;
-                        case "-":
-                            result += n1 - n2;
-                            break;
-                        case "/":
-                            result += n1 / n2;
-                            break;
-                        case "*":
-                            result += n1 * n2;
-                            break;
-                        case "%":
-                            result += n1 % n2;
-                            break;
-                        case "d":
-                            result += Math.Pow(n1, n2);
-                            break;
-                        default:
-                            Console.ForegroundColor = ConsoleColor.Red;
-                            Console.WriteLine("It isn't possible in the program");
-                            Console.ResetColor();
-                            break;
-                    }
-
-                    if (Error.Length == 0)
-                    {
-                        Console.WriteLine(result);
-                    }
-                    else
-                    {
-                        Console.WriteLine(result);
-                        RedErrorString();
-                    }
+                    Operations(oper);
 
                     Console.ForegroundColor = ConsoleColor.Cyan;
                     Console.WriteLine("Press S to stop and exit");
@@ -96,18 +54,8 @@ namespace calculator
                 }
                 else if (oper == "m")
                 {
-                    double n = GetDoubleFromUser("Number: ");
-
-                    if (n < 0 || n >= 0)
-                    {
-                        Console.WriteLine($"|{Math.Abs(n)}|");
-
-                    }
-                    else
-                    {
-                        RedErrorString();
-                    }
-
+                    ModulOperation();  
+  
                     Console.ForegroundColor = ConsoleColor.Cyan;
                     Console.WriteLine("Press S to stop and exit");
 
@@ -126,7 +74,67 @@ namespace calculator
                     Console.ResetColor();
                 }
             }
-        }       
+        }         
+        static void Operations(string oper)
+        {
+            double n1 = GetDoubleFromUser("Number 1: ");
+
+            double n2 = GetDoubleFromUser("Number 2: ");
+
+            string result = "Result: ";
+            string Error = "";
+
+            switch (oper)
+            {
+                case "+":
+                    result += n1 + n2;
+                    break;
+                case "-":
+                    result += n1 - n2;
+                    break;
+                case "/":
+                    result += n1 / n2;
+                    break;
+                case "*":
+                    result += n1 * n2;
+                    break;
+                case "%":
+                    result += n1 % n2;
+                    break;
+                case "d":
+                    result += Math.Pow(n1, n2);
+                    break;
+                default:
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("It isn't possible in the program");
+                    Console.ResetColor();
+                    break;
+            }
+
+            if (Error.Length == 0)
+            {
+                Console.WriteLine(result);
+            }
+            else
+            {
+                Console.WriteLine(result);
+                RedErrorString();
+            }
+        }
+        static void ModulOperation()
+        {
+            double n = GetDoubleFromUser("Number: ");
+
+            if (n < 0 || n >= 0)
+            {
+                Console.WriteLine($"|{Math.Abs(n)}|");
+
+            }
+            else
+            {
+                RedErrorString();
+            }
+        }
         static void RedErrorString()
         {
             Console.ForegroundColor = ConsoleColor.Red;
