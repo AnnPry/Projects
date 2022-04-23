36cc7f1a98363c87f68ddd4f182e4a2a2886bb52
AnnPry <87533389+AnnPry@users.noreply.github.com>
2022-04-23 15:01:51 +0300

    .

---
+++ Menu/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+
+namespace Menu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = GetUserName();
+            Console.WriteLine(name);
+
+            Console.WriteLine("Menu");
+            Console.WriteLine();
+
+            Console.WriteLine("1. Profile");
+            Console.WriteLine("2. Contacts");
+            Console.WriteLine("3. Settings");
+
+            Console.WriteLine();
+
+            int number = GetNumberOfUser();
+            Console.WriteLine(number);
+        }
+        static string GetUserName()
+        {
+            string name;
+            do
+            {
+                Console.Write("Enter your name: ");
+                name = Console.ReadLine();
+
+                if (name.Length >= 2 && int.TryParse(name, out int qwe) == false)
+                {
+                    Console.WriteLine($"Welcome, {name}");
+                    Console.ReadLine();
+
+                    Console.Clear();
+                    break;
+                }
+                else 
+                {
+                    Console.WriteLine("Error");
+                    Console.ReadLine();
+
+                    Console.Clear();
+                }
+            }
+            while (true);
+
+            return name;
+        }
+
+        static int GetNumberOfUser()
+        {
+            int n = 0;
+            do
+            {
+                Console.Write("Enter number of Menu: ");
+                bool number = int.TryParse(Console.ReadLine(), out int num);
+
+                if (number == true && num == 1)
+                {
+                    Console.Clear();
+                    Console.WriteLine("You chose Profile");
+                    break;
+                }
+                else if (number == true && num == 2)
+                {
+                    Console.Clear();
+                    Console.WriteLine("You chose Contacts");
+                    break;
+                }
+                else if (number == true && num == 3)
+                {
+                    Console.Clear();
+                    Console.WriteLine("You chose Settings");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Error");
+                }
+
+            }
+            while (true);
+
+            return n;
+        }
+    }
+}
--- X and 0/Program.cs
+++ X and 0/Program.cs
@@ -20,12 +20,13 @@ namespace X_and_0
 
             while (Has0(xAnd0))            
             {
+                int vertical = GetCoordinatesFromUser("Vertical coordinate: ");
+                //Console.Write("Vertical coordinate: ");
+                //bool vertical = int.TryParse(Console.ReadLine(), out int vrt);
 
-                Console.Write("Vertical coordinate: ");
-                bool vertical = int.TryParse(Console.ReadLine(), out int vrt);
-
-                Console.Write("Horizontal coordinate: ");
-                bool horizontal = int.TryParse(Console.ReadLine(), out int hzt);
+                int horizontal = GetCoordinatesFromUser("Horizontal coordinate: ");
+                //Console.Write("Horizontal coordinate: ");
+                //bool horizontal = int.TryParse(Console.ReadLine(), out int hzt);
 
                 //int min = 0;
                 //int max = 2;
@@ -37,15 +38,15 @@ namespace X_and_0
                 //    Console.ResetColor();
                 //}
 
-                if (vertical == true && horizontal == true)
-                {
+                //if (vertical == true && horizontal == true)
+                //{
 
                     for (int i = 0; i < xAnd0.GetLength(0); i++)
                     {
                         for (int k = 0; k < xAnd0.GetLength(1); k++)
                         {
                             int x = 1;
-                            xAnd0[vrt, hzt] = x;
+                            xAnd0[vertical, horizontal] = x;
                         }
                     }
                     for (int i = 0; i < xAnd0.GetLength(0); i++)
@@ -56,22 +57,25 @@ namespace X_and_0
                         }
                         Console.WriteLine();
                     }
-                }
-                else 
-                {
-                    if (vertical == false || horizontal == false)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("ERROR");
-                        Console.ResetColor();
-                    }
-                }
+                //}
+                //else 
+                //{
+                //    //if (vertical == false || horizontal == false)
+                //    //{
+                //        Console.ForegroundColor = ConsoleColor.Red;
+                //        Console.WriteLine("ERROR");
+                //        Console.ResetColor();
+                //    //}
+                //}
 
-                Console.Write("Vertical coordinate: ");
-                bool vertical2 = int.TryParse(Console.ReadLine(), out int vrt2);
+                int vertical2 = GetCoordinatesFromUser("Vertical coordinate: ");
 
-                Console.Write("Horizontal coordinate: ");
-                bool horizontal2 = int.TryParse(Console.ReadLine(), out int hzt2);
+                int horizontal2 = GetCoordinatesFromUser("Horizontal coordinate: ");
+                //Console.Write("Vertical coordinate: ");
+                //bool vertical2 = int.TryParse(Console.ReadLine(), out int vrt2);
+
+                //Console.Write("Horizontal coordinate: ");
+                //bool horizontal2 = int.TryParse(Console.ReadLine(), out int hzt2);
 
                 //int min2 = 0;
                 //int max2 = 2;
@@ -83,8 +87,8 @@ namespace X_and_0
                 //    Console.ResetColor();                   
                 //}
 
-                if (vertical2 == true && horizontal2 == true)
-                {
+                //if (vertical2 == true && horizontal2 == true)
+                //{
                     for (int l = 0; l < xAnd0.GetLength(0); l++)
                     {
                         for (int n = 0; n < xAnd0.GetLength(1); n++)
@@ -92,7 +96,7 @@ namespace X_and_0
                             for (int m = 0; m < 3; m++)
                             {
                                 int O = 2;
-                                xAnd0[vrt2, hzt2] = O;
+                                xAnd0[vertical2, horizontal2] = O;
                             }
                         }
                     }
@@ -105,16 +109,16 @@ namespace X_and_0
                         }
                         Console.WriteLine();
                     }
-                }
-                else
-                {
-                    if( vertical2 == false || horizontal2 == false)
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("ERROR");
-                        Console.ResetColor();
-                    }
-                }
+                //}
+                //else
+                //{
+                //    if( vertical2 == false || horizontal2 == false)
+                //    {
+                //        Console.ForegroundColor = ConsoleColor.Red;
+                //        Console.WriteLine("ERROR");
+                //        Console.ResetColor();
+                //    }
+                //}
             }           
         }
         static bool Has0(int[,] qwe)
@@ -130,7 +134,27 @@ namespace X_and_0
                     }
                 }
             }
-            return false;
+            return false;           
+        }
+        static int GetCoordinatesFromUser(string TextLine)
+        {
+            bool something = false;
+            int number = 0;
+
+            while (!something )
+            {
+                Console.Write(TextLine);
+                string input = Console.ReadLine();
+                something = int.TryParse(input, out number);
+
+                if (!something)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Input isn't a number! Plese try again!");
+                    Console.ResetColor();
+                }
+            }
+            return number;
         }
     }
 }
